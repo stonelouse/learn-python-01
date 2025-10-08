a696db81d5dc16d77b523b40fbf21c38b2293488
Christian Russ <russ.c@eplan.de>
2025-10-08 07:08:41 +0200

    feat: Defining the bare ' Phrase' class

---
+++ chp007_objects_classes/package/palindrome.py
@@ -0,0 +1,12 @@
+class Phrase:
+  """A class to represent phrases."""
+
+# The next line arranges
+# … to execute the subsequent code
+# … if the file is run at the command line
+# … but not when the class is loaded into other files
+# … see https://docs.python.org/3/library/__main__.html
+if __name__ == "__main__":
+  # creating a Phrase instance
+  phrase = Phrase()
+  print(phrase)
