44c9dd62cc7a5ca2b5f52fbd2ade9bd991f9dc03
Christian Russ <c.russ@web.de>
2025-10-09 06:51:26 +0200

    feat: Add 'palindrome' function

--- chp007_objects_classes/package/palindrome.py
+++ chp007_objects_classes/package/palindrome.py
@@ -4,15 +4,11 @@ class Phrase:
   def __init__(self, content):
     self.content = content
 
-# The next line arranges
-# … to execute the subsequent code
-# … if the file is run at the command line
-# … but not when the class is loaded into other files
-# … see https://docs.python.org/3/library/__main__.html
-if __name__ == "__main__":
-  # creating a Phrase instance
-  phrase = Phrase("Madam, I'm Adam")
-  print(phrase.content)
+def reverse(string):
+  """Reverse a string."""
+  return "".join(reversed(string))
 
-  phrase.content = "Able was I, ere I saw Elba"
-  print(phrase.content)
+def ispalindrome(string):
+  """Return True for a palindrome, False otherwise."""
+  processed_content = string.lower()
+  return processed_content == reverse(processed_content)
