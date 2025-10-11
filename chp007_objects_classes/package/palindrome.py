3d5b48d4425f96ef96e7b1d7beda1a5c453bc13d
Christian Russ <c.russ@web.de>
2025-10-11 06:41:16 +0200

    feat: Add an iterator to the 'Phrase' class

--- chp007_objects_classes/package/palindrome.py
+++ chp007_objects_classes/package/palindrome.py
@@ -9,6 +9,13 @@ class Phrase:
     processed_content = self.content.lower()
     return processed_content == reverse(processed_content)
 
+  def __iter__(self):
+    self.phrase_iterator = iter(self.content)
+    return self
+  
+  def __next__(self):
+    return next(self.phrase_iterator)
+
 def reverse(string):
   """Reverse a string."""
   return "".join(reversed(string))
