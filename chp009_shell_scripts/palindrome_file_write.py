850c54374836701245d427ec98852163b798f3d9
Christian Russ <c.russ@web.de>
2025-11-21 19:02:47 +0100

    feat: Follow convention for  Python scripts

--- chp009_shell_scripts/palindrome_file_write.py
+++ chp009_shell_scripts/palindrome_file_write.py
@@ -2,11 +2,15 @@
 
 from palindrome_stonelouse.phrase import Phrase
 
-with open("phrases.txt") as file:
-    palindromes = [line for line in file.readlines() 
-                    if Phrase(line).ispalindrome()]
+def main():
+  with open("phrases.txt") as file:
+      palindromes = [line for line in file.readlines() 
+                      if Phrase(line).ispalindrome()]
 
-with open("palindromes.txt", "w") as pfile:
-    for p in palindromes:
-        pfile.write(p)
-        # Note: 'p' already includes its own newline character
+  with open("palindromes.txt", "w") as pfile:
+      for p in palindromes:
+          pfile.write(p)
+          # Note: 'p' already includes its own newline character
+
+if __name__ == "__main__":
+    main()
