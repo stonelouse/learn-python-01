c0d6f806a1ecb5a721f757348645597ad385d15c
Christian Russ <c.russ@web.de>
2025-11-22 08:10:31 +0100

    feat: Read phrases from URL and write palindromes to a file

---
+++ chp009_shell_scripts/palindrome_url_read_file_write.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import requests
+from palindrome_stonelouse.phrase import Phrase
+
+def main():
+  url = "https://cdn.learnenough.com/phrases.txt"
+
+  response = requests.get(url)
+  palindromes = [line for line in response.text.splitlines(keepends=True)
+                  if Phrase(line).ispalindrome()]
+
+  with open("palindromes_from_url.txt", "w") as pfile:
+      for p in palindromes:
+          pfile.write(p)
+          # Note: 'p' already includes its own newline character
+
+if __name__ == "__main__":
+    main()
