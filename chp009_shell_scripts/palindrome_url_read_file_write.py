e71087344117e9ff6c24a186967e7f88550d2d90
Christian Russ <c.russ@web.de>
2025-11-22 08:16:10 +0100

    feat: Fix encoding issues and format source file

--- chp009_shell_scripts/palindrome_url_read_file_write.py
+++ chp009_shell_scripts/palindrome_url_read_file_write.py
@@ -3,17 +3,22 @@
 import requests
 from palindrome_stonelouse.phrase import Phrase
 
+
 def main():
-  url = "https://cdn.learnenough.com/phrases.txt"
+    url = "https://cdn.learnenough.com/phrases.txt"
 
-  response = requests.get(url)
-  palindromes = [line for line in response.text.splitlines(keepends=True)
-                  if Phrase(line).ispalindrome()]
+    response = requests.get(url)
+    palindromes = [
+        line
+        for line in response.content.decode("utf-8").splitlines(keepends=True)
+        if Phrase(line).ispalindrome()
+    ]
 
-  with open("palindromes_from_url.txt", "w") as pfile:
-      for p in palindromes:
-          pfile.write(p)
-          # Note: 'p' already includes its own newline character
+    with open("palindromes_from_url.txt", "w") as pfile:
+        for p in palindromes:
+            pfile.write(p)
+            # Note: 'p' already includes its own newline character
+
 
 if __name__ == "__main__":
     main()
